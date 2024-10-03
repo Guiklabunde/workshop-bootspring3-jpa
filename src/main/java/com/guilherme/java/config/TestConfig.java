51bb84c3e71ec9e36544d43018ebb0a0e8f8fc6b
Guilherme Andrei Klabunde <guiandrei17@gmail.com>
2024-10-03 14:33:57 -0300

    Product

--- src/main/java/com/guilherme/java/config/TestConfig.java
+++ src/main/java/com/guilherme/java/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.guilherme.java.entities.Category;
 import com.guilherme.java.entities.Order;
+import com.guilherme.java.entities.Product;
 import com.guilherme.java.entities.User;
 import com.guilherme.java.entities.enums.OrderStatus;
 import com.guilherme.java.repositories.CategoryRepository;
 import com.guilherme.java.repositories.OrderRepository;
+import com.guilherme.java.repositories.ProductRepository;
 import com.guilherme.java.repositories.UserRepository;
 
 @Configuration
@@ -29,7 +31,9 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
-	
+	@Autowired
+	private ProductRepository productRepository;
+		
 	@Override
 	public void run(String... args) throws Exception {
 		
@@ -37,7 +41,14 @@ public class TestConfig implements CommandLineRunner {
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 		
+		Product p1 = new Product(null, "The Lord of the Rings", "Buscando o anel perdido", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Lorem Ipsum", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Sem Mac por enquanto", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Saudade de Jogar FIFA", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cree en grande", 100.99, "");
+		
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "99999", "12345");
 		User u2 = new User(null, "Xablau Silva", "xs@gmail.com", "989999", "123456");
--- src/main/java/com/guilherme/java/entities/Category.java
+++ src/main/java/com/guilherme/java/entities/Category.java
@@ -1,13 +1,16 @@
 package com.guilherme.java.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 
 @Entity
@@ -19,6 +22,10 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Transient
+	private Set<Product> products = new HashSet<>();
+	
 	
 	public Category() {
 		
@@ -45,10 +52,17 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public Set<Product> getProducts() {
+		return products;
+	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -62,5 +76,6 @@ public class Category implements Serializable {
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
+		
 }
